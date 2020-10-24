dc5a86de0d43775e503d0c39f1ec48f4d189bdd9
inmyth <martin.bcu@gmail.com>
2020-10-24 13:35:51 +0900

    stopped work on verifying all AWS resources with IAM SDK

--- src/main/scala/mbcu/me/Main.scala
+++ src/main/scala/mbcu/me/Main.scala
@@ -7,18 +7,21 @@ import mbcu.me.domain.services.certivmanagement.FileRepo.S3Path
 import pureconfig._
 import pureconfig.generic.auto._
 import mbcu.me.config.ConfUtils._
+
 import scala.concurrent.Await
 import scala.concurrent.duration.DurationInt
 
 object Main extends App {
 
+  println(a)
   val config = ConfigSource.default.load[Config]
 
   config match {
     case Right(value) => {
       val x = Repositories.fromConfig(value)
 
-//      val z = x.certivStorage.put(S3Path("aaa", "b.txt"), new File("README.md")).runToFuture(ExecutorsConfig.ecIO)
+//      val z =
+//        x.certivStorage.put(S3Path(Some(Seq("aaa")), "b.txt"), new File("README.md")).runToFuture(ExecutorsConfig.ecIO)
       val z = x.certivStorage
         .putDeleteIAMTestFile(value.repositoryConfig.s3Config.iamTestFilePath)
         .runToFuture(ExecutorsConfig.ecIO)
